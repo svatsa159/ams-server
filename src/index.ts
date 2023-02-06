9ac0f0bd58693caacd6130849fb98e07c10e61c0
svatsa159 <svatsa159@gmail.com>
2023-02-06 23:03:51 +0530

    Setup Details

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,25 @@
 console.log('Hello world!')
+
+//POST - Register user
+//GET - All Users
+//POST - Mark attendance of user
+//POST - Login - JWT
+//GET - Single User's attendance
+
+
+
+interface User{
+    _id:string,
+    username: string,
+    name:string,
+    dob: string,
+    city: string,
+    class: string,
+    password: string,
+    isAdmin: boolean,
+}
+
+interface Attendance {
+    date: string,
+    users: string[] //userId
+}
